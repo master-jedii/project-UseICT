ac55571d69aa4cd53ab1e4282ab7510e26898f5c
Mossy112 <163358716+Mossy112@users.noreply.github.com>
2025-01-03 19:21:01 +0700

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -11,8 +11,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Login />
+    <Navbar/>
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
