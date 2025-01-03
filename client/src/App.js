aa1aac303c44dda9857243c37664378f868ea64c
PondPurim <pondpond1144@gmail.com>
2025-01-03 17:39:35 +0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React fdsfsfsfsfsfs
+          Learn React 
         </a>
       </header>
     </div>
