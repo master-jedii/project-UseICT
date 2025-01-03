7d607477dee3ef719703d7601009e71c92fbecc3
master-jedii <pemakon.vlr@gmail.com>
2025-01-03 19:02:46 +0700

    login

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Login from './components/login';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css'
@@ -9,7 +10,7 @@ import Navbar from './components/Navbar';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Navbar />
+    <Login />
   </React.StrictMode>
 );
 
