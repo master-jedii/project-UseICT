319f5d6137a0e6b7215ef2f6c07b25bb301c82b1
master-jedii <pemakon.vlr@gmail.com>
2025-01-04 16:39:43 +0700

    แก้ error class JSX

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -7,7 +7,7 @@ const Carousel = () => {
     <div id='imgpromote'>
         <p>การให้บริการยืม-คืนอุปกรณ์ กำหนดเปิด/ปิด วันจันทร์-วันศุกร หยุดเสาร์-อาทิตย์และวันหยุดนักขัตฤกษ์เวลา <br></br>09.00-16.30 น.  </p>
         
-       <button class='btn-borrow'>ยืมคืนอุปกรณ์</button>
+       <button className='btn-borrow'>ยืมคืนอุปกรณ์</button>
     </div>
   )
 }
