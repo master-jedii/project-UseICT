6668bfc51448b61770f0c9c70cff6baf90a4ab07
Mossy112 <163358716+Mossy112@users.noreply.github.com>
2025-01-04 17:07:17 +0700

    AdminPage  กูยังทำเงอะๆอย่าเพิ่งเอาไปใส่

---
+++ client/src/Admin/Admin.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import AdminPage from '../components/AdminPage';
+
+const Admin = () => {
+  return (
+  <>
+    <AdminPage/>
+    
+    {/* <div>
+      <h1>Admin Page</h1>
+      <h2>Welcome to the Admin Dashboard!</h2>
+    </div> */}
+  </>
+    );
+};
+
+export default Admin;
--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Carousel from './components/Carousel';
 import Login from './components/login';
+import Admin from './Admin/Admin';
 
 const MainLayout = ({ children }) => (
   <>
@@ -26,6 +27,7 @@ const App = () => {
         />
         {/* เส้นทางที่ไม่มี Navbar */}
         <Route path="/login" element={<Login />} />
+        <Route path="/Admin" element={<Admin />} />
       </Routes>
     </Router>
   );
