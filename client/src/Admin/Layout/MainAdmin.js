3a1601d479012799b03086805f5aca9cf8ac36a3
PondPurim <pondpond1144@gmail.com>
2025-01-05 19:34:03 +0700

    Admin

---
+++ client/src/Admin/Layout/MainAdmin.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import'../CSS/MainAdmin.css'
+const MainAdmin = () => {
+  return (
+    
+    <div className='Adminss'>
+      MainAdmin
+    </div>
+  )
+}
+
+export default MainAdmin
