7601effe84ef6aef4af072c34db82265a3f7ff73
PondPurim <pondpond1144@gmail.com>
2025-01-05 23:17:06 +0700

    inter

--- client/src/interface/DisplayEquipment.js
+++ client/src/interface/DisplayEquipment.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Axios from 'axios';
-
+import '../View/DisplayEquipment.css'
 const DisplayEquipment = () => {
   const [equipment, setEquipment] = useState([]);
 
@@ -15,12 +15,14 @@ const DisplayEquipment = () => {
 
   return (
     <div className="equipment-list">
-      
-      <div>
+
+      <div className='header-category'>
         <h1>กล้อง</h1>
+
         
+
       </div>
-      
+
     </div>
   );
 };
