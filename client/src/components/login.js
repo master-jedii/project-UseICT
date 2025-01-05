5b7e61dba2a2f16236bbbd205b443e45d515f959
Mossy112 <163358716+Mossy112@users.noreply.github.com>
2025-01-05 18:28:26 +0700

    Ui Login

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -4,6 +4,8 @@ import Swal from "sweetalert2";
 import { useNavigate } from "react-router-dom"; // ใช้ useNavigate เพื่อทำการ redirect
 import '../View/Login.css'
 
+
+
 const Login = () => {
   const [formData, setFormData] = useState({
     email: "",
@@ -43,7 +45,11 @@ const Login = () => {
   return (
     <div className="login-container">
       <div className="login-left">
-        {/* พื้นที่ด้านซ้ายใช้สำหรับรูปพื้นหลัง */}
+        <img
+            src="/logo.png"
+            alt="SU Kits Logo"
+            className="logo-login"
+          />
       </div>
       <div className="login-right">
         <form className="login-form" onSubmit={handleLogin}>
